094383882ce67d6485762ac29ac9d3804eff1cac
Lee Lorenz Sr <LeeLorenzSr@gmail.com>
2020-08-07 10:13:49 -0400

    Updated project file to support multi-targeting
    Version 1.0.2

---
+++ IniGetter/Reflection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection;
+
+namespace IniGetter
+{
+    /// <summary>
+    /// Handle the reflection differences between the full .net framework and
+    /// what is provided by netstandard
+    /// </summary>
+    public static class Reflection
+    {
+        public static Assembly GetAssembly(this Type type)
+        {
+#if (NET35 || NET40 || NET45 || NET451 || NET46 || NET461)
+            return type.Assembly;
+#else
+            return type.GetTypeInfo().Assembly;
+#endif
+        }
+    }
+}
